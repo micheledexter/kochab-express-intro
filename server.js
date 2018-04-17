cf9969f49482712226a91a7155b31c9f69b414a8
Michele Dexter <micheleannedexter@gmail.com>
2018-04-17 11:52:22 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+// 'req' is "request", 'res' is "response"
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/hey', function (req, res) {
+    res.send('Howdy');
+});
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`)
+});
